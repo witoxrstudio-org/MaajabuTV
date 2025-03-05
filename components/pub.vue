93a97e732e84b9b3baafde6d82cbc79eeb6976c9
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2025-03-05 13:41:30 +0200

    here

---
+++ components/pub.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="relative">
+    <!-- Section avec effet de parallaxe -->
+    <div
+      class="parallax h-48 w-full bg-cover bg-center"
+      style="background-image: url('/img/cover.jpg')"
+    ></div>
+  </div>
+</template>
+
+<style>
+.parallax {
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: center;
+}
+</style>
