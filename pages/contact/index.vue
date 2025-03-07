57976e99bc3da768c71f4304efcd38191550afeb
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2025-03-07 13:26:26 +0200

    copy

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,23 +1,31 @@
 <template>
   <div class="relative w-full bg-black">
+    <!-- Section avec image de fond -->
     <section
-      class="relative w-full h-[300px] bg-cover bg-center"
+      class="relative w-full h-[300px] bg-cover bg-center flex items-center md:h-[300px] sm:h-[200px]"
       style="background-image: url('/img/luk.jpg')"
     >
       <div
         class="absolute inset-0 bg-gradient-to-t from-black to-transparent"
       ></div>
+      <div class="relative z-10 ml-6 text-white mj-container mt-28">
+        <h1 class="text-4xl font-bold sm:text-3xl">
+          Notre télévision en direct est diffusée sur Plex
+        </h1>
+        <p class="mt-2 text-gray-300 sm:text-sm text-xs">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
     </section>
 
     <!-- Contenu principal -->
     <section class="bg-black text-white py-8">
       <div class="mj-container bg-black px-4">
-        <p class="text-justify">
+        <p class="text-justify sm:text-sm text-xs">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
           eius iure commodi, accusamus error incidunt quasi soluta velit dolore
           delectus pariatur aliquid sed blanditiis praesentium magnam voluptatum
           sint harum repellendus. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit.
         </p>
       </div>
     </section>
