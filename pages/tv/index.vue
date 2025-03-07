055f7f59b75c50d03aed5b57448c690d3da0a319
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2025-03-07 10:18:35 +0200

    here

--- pages/tv/index.vue
+++ pages/tv/index.vue
@@ -1,20 +1,25 @@
 <template>
-  <section class="relative w-full bg-black">
-    <!-- Image de fond enlevée -->
-    <div
-      class="absolute inset-0 bg-gradient-to-t from-black to-transparent bg-[length:200%_100%] animate-smoke"
-    ></div>
-    <div
-      class="relative mj-container z-10 flex flex-col items-start justify-center pt-28 text-white text-center px-4"
+  <div class="relative w-full bg-black">
+    <section
+      class="relative w-full h-[300px] bg-cover bg-center"
+      style="background-image: url('/img/luk.jpg')"
     >
-      <p class="text-justify">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        eius iure commodi, accusamus error incidunt quasi soluta velit dolore
-        delectus pariatur aliquid sed blanditiis praesentium magnam voluptatum
-        sint harum repellendus.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit.
-      </p>
-    </div>
-  </section>
-</template>
+      <div
+        class="absolute inset-0 bg-gradient-to-t from-black to-transparent"
+      ></div>
+    </section>
 
+    <!-- Contenu principal -->
+    <section class="bg-black text-white py-8">
+      <div class="mj-container bg-black px-4">
+        <p class="text-justify">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+          eius iure commodi, accusamus error incidunt quasi soluta velit dolore
+          delectus pariatur aliquid sed blanditiis praesentium magnam voluptatum
+          sint harum repellendus. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit.
+        </p>
+      </div>
+    </section>
+  </div>
+</template>
