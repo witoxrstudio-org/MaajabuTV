ecb47211ab6c37804341748ebf934c6d17e83857
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2025-03-07 13:12:43 +0200

    that

--- components/brands/index.vue
+++ components/brands/index.vue
@@ -1,20 +1,25 @@
 <template>
   <div
-    class="mj-container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
+    class="mj-container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4"
   >
     <img
-      class="max-h-24 w-full object-contain transition-all duration-300 hover:scale-110"
+      class="max-h-24 object-contain transition-all duration-300 hover:scale-110"
       src="/img/d1.png"
       alt="Transistor"
     />
     <img
-      class="max-h-24 w-full object-contain transition-all duration-300 hover:scale-110"
+      class="max-h-24 object-contain transition-all duration-300 hover:scale-110"
       src="/img/d3.png"
       alt="Reform"
     />
     <img
-      class="max-h-24 w-full object-contain transition-all duration-300 hover:scale-110"
+      class="max-h-24 object-contain transition-all duration-300 hover:scale-110"
       src="/img/d2.png"
+      alt="Tuple"
+    />
+    <img
+      class="max-h-24 object-contain transition-all duration-300 hover:scale-110"
+      src="/img/mg.png"
       alt="Tuple"
     />
   </div>
