4997652932946fb5cfe183cd907bfeb73859d45b
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2025-03-20 22:51:40 +0200

    thats

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  vue: {
+    compilerOptions: {
+      isCustomElement: (tag) => tag.startsWith("NuxtTransition"),
+    },
+  },
   css: [
     "~/assets/css/main.css",
     "plyr/dist/plyr.css",
