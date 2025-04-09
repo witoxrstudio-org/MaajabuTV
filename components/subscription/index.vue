699bd05fce28a0667189980906fb90bd50f5d0fd
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2025-04-09 10:00:17 +0200

    HereWeAre

--- components/subscription/index.vue
+++ components/subscription/index.vue
@@ -13,18 +13,54 @@
     <!-- Content -->
     <div class="relative z-10">
       <div class="mj-container">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-        eligendi quibusdam nostrum amet omnis tempore nemo, quasi optio qui
-        quas. Earum assumenda omnis enim minima mollitia ex voluptate, at
-        sit!Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-        eligendi quibusdam nostrum amet omnis tempore nemo, quasi optio qui
-        quas. Earum assumenda omnis enim minima mollitia ex voluptate, at
-        sit!Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-        elig
+        <div class="flex justify-between items-center py-4 md:px-0">
+          <div class="w-1/3 border-t-2 border-white"></div>
+          <div class="w-1/3 text-center hidden md:block"></div>
+          <div class="w-1/3 border-t-2 border-white"></div>
+        </div>
+        <div class="flex items-center justify-between w-full mx-auto">
+          <h2 class="text-2xl md:text-3xl font-bold text-white relative group">
+            <span class="text-blue-500">Abonnez-vous</span> à notre chaîne
+          </h2>
+          <a
+            href="#"
+            class="text-white font-medium text-sm md:text-base relative group"
+          >
+            Voir plus
+            <span
+              class="absolute left-0 bottom-0 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"
+            ></span>
+          </a>
+        </div>
+        <div class="my-8">
+          <p class="text-center text-white text-lg mb-4">
+            Rejoignez la communauté Maajabu TV et ne manquez jamais nos
+            dernières actualités ! En vous abonnant, vous recevrez des mises à
+            jour exclusives sur nos programmes, des offres spéciales et des
+            contenus inspirants directement dans votre boîte mail. Restez
+            connecté avec nous et découvrez le meilleur de Maajabu TV.
+          </p>
+
+          <form
+            name="email-form"
+            class="relative mx-auto mb-4 flex w-full max-w-2xl flex-col items-start justify-center sm:flex-row"
+          >
+            <input
+              type="email"
+              class="mb-3 mr-6 block h-9 w-full bg-white px-6 py-7 text-sm text-[#333333] focus:border-[#3898ec]"
+              placeholder="Entrez votre email"
+              required=""
+            />
+            <input
+              type="submit"
+              value="Subscribe"
+              class="inline-block w-full cursor-pointer bg-[#276ef1] px-6 py-3 text-center font-semibold text-white transition [box-shadow:rgb(171,_196,_245)_-8px_8px] hover:[box-shadow:rgb(171,_196,_245)_0px_0px] sm:w-28"
+            />
+          </form>
+        </div>
       </div>
     </div>
 
-    <!-- Footer Blur Bar -->
     <div class="backdrop-blur bg-white/10 py-6 w-full z-10">
       <div
         class="mj-container mx-auto flex flex-col md:flex-row items-center justify-between px-4"
@@ -35,17 +71,22 @@
         </p>
         <div class="flex items-center space-x-6 text-2xl">
           <NuxtLink to="#" aria-label="Facebook"
-            ><i class="fab fa-facebook-f"></i
+            ><i class="fab fa-facebook-f hover:text-blue-400"></i
           ></NuxtLink>
-          <NuxtLink to="#" aria-label="Instagram"
-            ><i class="fab fa-instagram"></i
+          <NuxtLink to="#" class="group" aria-label="Instagram">
+            <i
+              class="fab fa-instagram text-white transition duration-300 group-hover:text-transparent group-hover:bg-clip-text group-hover:bg-gradient-to-r group-hover:from-red-500 group-hover:to-orange-500"
+            >
+            </i
           ></NuxtLink>
-          <NuxtLink to="#" aria-label="TikTok"
-            ><i class="fab fa-tiktok"></i
+          <NuxtLink to="#" class="group" aria-label="TikTok"
+            ><i
+              class="fab fa-tiktok text-white transition duration-300 group-hover:text-transparent group-hover:bg-clip-text group-hover:bg-gradient-to-r group-hover:from-black group-hover:to-white"
+            ></i
           ></NuxtLink>
 
           <NuxtLink to="#" aria-label="YouTube"
-            ><i class="fab fa-youtube"></i
+            ><i class="fab fa-youtube hover:text-red-700"></i
           ></NuxtLink>
         </div>
       </div>
